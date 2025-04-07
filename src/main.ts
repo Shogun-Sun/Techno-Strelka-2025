786a7eab5def87ade2d96a4bc8078ebe73fb1d4d
Shogun_Sun <fima8516@gmail.com>
2025-04-07 20:31:38 +0300

    add static logic, swagger and views engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { PUBLIC_PATH, VIEWS_PATH } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(PUBLIC_PATH);
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(VIEWS_PATH);
+
+  if (process.env.PROJECT_STATUS === 'dev') {
+    const swaggerDoc = new DocumentBuilder()
+      .setTitle('Swagger')
+      .setDescription('Документаци для API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerDoc);
+    SwaggerModule.setup('/api', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
