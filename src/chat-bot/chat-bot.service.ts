191e92405249e628cc905beb66c8b2b9e6c25eec
Shogun_Sun <fima8516@gmail.com>
2025-04-09 14:53:59 +0300

    Refresh context text

--- src/chat-bot/chat-bot.service.ts
+++ src/chat-bot/chat-bot.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { DialogPromptDto } from './dto/dialogPrompt.dto';
+import { CONTEXT } from 'src/config';
 
 @Injectable()
 export class ChatBotService {
@@ -16,17 +17,7 @@ export class ChatBotService {
   async accessGeminiApi(
     prompt: DialogPromptDto,
   ): Promise<{ message: string; redirect?: string }> {
-    const context = `
-    Ты — оператор T2 (у тебя нет пола), работаешь с абонентами компании.
-    Твоя задача — помогать пользователям в вопросах связи, тарифах и прочих вещей, связанных с оператором.
-    Ты владеешь полной информацией о тарифах, акциях и услугах компании.
-    Но на каждое сообщение надо отвечать не более чем 100 словами.
-    Хоть ты и оператор связи Т2, но ты не можешь работать с номерами телефона, смотреть тариф и т.д, поэтому всегда говори такую фразу:
-    "Извинете, я не могу помочь с этим вопросом, но я переведу вас в личный кабинет абонента Т2 или позвоните на горячую линию" 
-    На все вопросы, которые не касаются оператора Т2, отвечай просто: Извините, я не могу отвечать на вопросы, которые не касаются моей задачи.
-    `;
-
-    const fullPrompt = `${context} ${prompt.prompt}`;
+    const fullPrompt = `${CONTEXT} ${prompt.prompt}`;
 
     try {
       const model = this.genAI.getGenerativeModel({
